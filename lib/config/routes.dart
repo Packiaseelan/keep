d32445b2f7699c70ca870db9aaeaed065cf9d638
Packiaseelan Socretes <pacsocre@WKMZTC2E6703.global.publicisgroupe.net>
2022-05-20 10:01:39 +0530

    splash screen added

---
+++ lib/config/routes.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:keep/ui/screens/screens.dart';
+
+class Routes {
+  static const String initial = '/';
+  static const String login = '/login';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case initial:
+        return _getPageRoute(
+          routeName: settings.name,
+          viewToShow: const SplashScreen(
+            key: Key('Rouetes_SplashScreen'),
+          ),
+        );
+
+      case login:
+        return _getPageRoute(
+          routeName: settings.name,
+          viewToShow: const LoginScreen(
+            key: Key('Rouetes_LoginScreen'),
+          ),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+
+  static PageRoute _getPageRoute({String? routeName, required Widget viewToShow}) {
+    return MaterialPageRoute(
+      settings: RouteSettings(
+        name: routeName,
+      ),
+      builder: (_) => viewToShow,
+    );
+  }
+}
--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:keep/config/config.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({ Key? key }) : super(key: key);
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -9,9 +12,27 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Timer(const Duration(seconds: 2), _navigationPage);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Image.asset('assets/icons/keep.png'),
+          Text('KEEP NOTES', style: Theme.of(context).textTheme.titleLarge)
+        ]),
+      ),
     );
+  }
+
+  void _navigationPage() {
+    Navigator.pushReplacementNamed(context, Routes.login);
   }
 }
